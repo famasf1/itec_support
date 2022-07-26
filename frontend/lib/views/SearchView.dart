3ec7bbf956b596762edc0c30f72d4fd9124767ed
jirayuth <jambo5167@gmail.com>
2022-07-27 02:53:07 +0700

    added searchview

--- frontend/lib/views/SearchView.dart
+++ frontend/lib/views/SearchView.dart
@@ -21,29 +21,54 @@ class _SearchPageState extends State<SearchPage> {
       });
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(appBar: AppBar(
-        title: Text("ITEC  Support"),
-        conterTitle: true,
-      ),
-      body: ListView(
-        children: Widget [
-          Padding: const EdgeInsets.all(10.0),
-          child: TextField(
-            onChanged = (val) {
-              searchResults.clear();
-              searchResultsDHLOpen(val)
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("ITEC  Support"),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: const <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                onChanged: (val) {
+                  //clearing result store in cache
+                  searchResults.clear();
+                  //then calling django for search query
+                  searchPostsDjango(val);
+                },
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(left: 25.0),
+                  hintText: 'Search Here',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  suffixIcon:
+                      IconButton(icon: Icon(Icons.search), onPressed: null),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: searchResults.length,
+              itemBuilder: (BuildContext context, int index) {
+                return buildResultCard(searchResults[index]);
+              },
+            ),
+          ],
 //TODO : Finish this piece of code
 //too tired and sleepy :/
 //https://medium.com/flutter-community/django-search-flutter-1cb3e8a5db1a
-            
-            }
-          )
-        ],))
-    )
+        ),
+      ),
+    );
   }
 }
